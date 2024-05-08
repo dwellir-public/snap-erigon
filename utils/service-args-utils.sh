ff2f05e4b3469249008745c35264e3730b4b2e42
ZeeshanMajeed <zeeshanmajeedabdul@gmail.com>
2024-05-08 11:30:06 +0200

    Fix issue for service args

--- utils/service-args-utils.sh
+++ utils/service-args-utils.sh
@@ -2,12 +2,12 @@
 
 . "$SNAP/utils/utils.sh"
 
-BASE_PATH="$SNAP_COMMON/polkadot_base"
+BASE_PATH="$SNAP_COMMON/erigon_base"
 SERVICE_ARGS_FILE="$SNAP_COMMON/service-arguments"
 
 write_service_args_file()
-{
-    service_args="--base-path=$BASE_PATH $(get_service_args)"
+{   
+    service_args="--datadir=$BASE_PATH $(get_service_args)"
     log "Writing \"$service_args\" to $SERVICE_ARGS_FILE"
     echo "$service_args" > "$SERVICE_ARGS_FILE"
 }
@@ -23,7 +23,6 @@ get_service_args()
     service_args="$(snapctl get service-args)"
     if [ -z "$service_args" ]; then
         log "Setting default service args"
-        service_args="--name=$(hostname)"
         # Don't use set_service_args() since it will not work when using snap unset
         snapctl set service-args="$service_args"
     fi
@@ -48,7 +47,7 @@ service_args_has_changed()
 validate_service_args()
 {
     case "$1" in 
-        *base-path*)
+         *--datadir*)
             log_message="base-path is not allowed to pass as a service argument restoring to last used service-args. This path is alywas used instead ${BASE_PATH}."
             log "$log_message"
             # Echo will be visible for a user if the configure hook fails when calling e.g. snap set SNAP_NAME service-args
